24cc00d5588d87af2b2ddd3c762746085a30533f
victor <gtamulty20@gmail.com>
2018-05-29 20:03:59 +0200

    ss

--- Startup.cs
+++ Startup.cs
@@ -55,10 +55,10 @@ namespace PSCORE
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-             app.UseForwardedHeaders(new ForwardedHeadersOptions {
+            app.UseForwardedHeaders(new ForwardedHeadersOptions {
                 ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto
             });
-            
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
